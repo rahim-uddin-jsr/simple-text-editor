193decb68f321e09b2647a9fd12ac8e80eb54630
dev-rahim <rahimuddin5915@gmail.com>
2023-02-15 00:37:01 +0600

    added some tyles

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const textArea = document.getElementById("text-area");
-document.getElementById("btn-style-bold").addEventListener("click", () => {
+const boldBtn = document.getElementById("btn-style-bold");
+boldBtn.addEventListener("click", (e) => {
   //   console.log((textArea.style.fontWeight = "bold"));
   if (textArea.style.fontWeight == "bold") {
     textArea.style.fontWeight = "normal";
+    boldBtn.style.border = "none";
   } else {
     textArea.style.fontWeight = "bold";
+    boldBtn.style.border = "2px solid #ddd";
   }
 });
-document.getElementById("btn-style-italic").addEventListener("click", () => {
+const italicBtn = document.getElementById("btn-style-italic");
+italicBtn.addEventListener("click", () => {
   //   textArea.style.fontStyle = "italic";
   if (textArea.style.fontStyle == "italic") {
     textArea.style.fontStyle = "normal";
+    italicBtn.style.border = "none";
   } else {
     textArea.style.fontStyle = "italic";
+    italicBtn.style.border = "2px solid #ddd";
   }
 });
 function styleToggle(property, firstValue, secondValue) {
@@ -22,28 +28,28 @@ function styleToggle(property, firstValue, secondValue) {
     textArea.style.property = firstValue;
   }
 }
-document.getElementById("btn-style-underline").addEventListener("click", () => {
+const btnUnderLine = document.getElementById("btn-style-underline");
+btnUnderLine.addEventListener("click", () => {
   if (textArea.style.textDecoration == "underline") {
     textArea.style.textDecoration = "none";
+    btnUnderLine.style.border = "none";
   } else {
     textArea.style.textDecoration = "underline";
+    btnUnderLine.style.border = "2px solid #ddd";
   }
 });
-document
-  .getElementById("btn-style-align-left")
-  .addEventListener("click", () => {
-    console.log((textArea.style.textAlign = "left"));
-  });
-document
-  .getElementById("btn-style-align-center")
-  .addEventListener("click", () => {
-    console.log((textArea.style.textAlign = "center"));
-  });
-document
-  .getElementById("btn-style-align-right")
-  .addEventListener("click", () => {
-    console.log((textArea.style.textAlign = "right"));
-  });
+const btnLeft = document.getElementById("btn-style-align-left");
+btnLeft.addEventListener("click", () => {
+  textArea.style.textAlign = "left";
+});
+const btnCenter = document.getElementById("btn-style-align-center");
+btnCenter.addEventListener("click", () => {
+  textArea.style.textAlign = "center";
+});
+const btnRight = document.getElementById("btn-style-align-right");
+btnRight.addEventListener("click", () => {
+  textArea.style.textAlign = "right";
+});
 function changeSize() {
   console.log("changed");
   const fontSize = document.getElementById("font-size").value;
